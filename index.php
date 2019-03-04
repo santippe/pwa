4155d0d9ff90ee507af3de14ae7c4bf576db18ee
Marco Mascioli <marco.mascioli@aesystech.it>
2019-03-04 18:13:51 +0100

    taaaaac

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <div id="searchdir">
     <input type="text" id="searchtxt">
+    <div id="searchhelp">
+        <div>Test 1</div>
+        <div>Test 2</div>
+        <div>Test 3</div>
+    </div>
 </div>
 <div id="news"></div>
 <div id="bottommenu"></div>
+<script>
+    let searchBox = document.querySelector('#searchtxt');
+    searchBox.addEventListener('click',()=>{
+        let searchTip = document.querySelector('#searchhelp');
+        searchTip.style.display = 'block';
+    },true);
+</script>
