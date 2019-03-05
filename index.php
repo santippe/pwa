4fb032a23b3f5cc1bedea1da45bc076f3456c878
Marco Mascioli <marco.mascioli@aesystech.it>
2019-03-05 17:50:42 +0100

    menu func

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <div id="searchdir">
     <input type="text" id="searchtxt">
     <div id="searchhelp">
-        <div>Test 1</div>
-        <div>Test 2</div>
-        <div>Test 3</div>
+        <a href="/">Test 1</a>
+        <a>Test 2</a>
+        <a>Test 3</a>
     </div>
 </div>
 <div id="news"></div>
 <div id="bottommenu"></div>
 <script>
     let searchBox = document.querySelector('#searchtxt');
+    let searchOpen = false;
     searchBox.addEventListener('click',()=>{
         let searchTip = document.querySelector('#searchhelp');
-        searchTip.style.display = 'block';
+        if (!searchOpen)            
+            searchTip.style.display = 'block';
+        else 
+            searchTip.style.display = 'none';
+        searchOpen = !searchOpen; 
     },true);
 </script>
